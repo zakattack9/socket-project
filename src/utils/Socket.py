aad8a798e80cf73637e6db6a9a8352378f5b093d
zakattack9 <zakattack26111@gmail.com>
2021-02-15 11:07:01 -0700

    improvements to create_socket

--- src/utils/Socket.py
+++ src/utils/Socket.py
@@ -8,6 +8,9 @@ def validate_port(port):
   return port
 
 def create_socket(port):
+  if (port > Constants.MAX_PORT_NUM):
+    port = Constants.MIN_PORT_NUM
+  new_socket = None
   try:
     # AF_INET indicates that the underlying network is using IPv4
     # SOCK_DGRAM indicates that it is a UDP socket
@@ -19,6 +22,7 @@ def create_socket(port):
   except Exception as err:
     if ('[Errno 98] Address already in use' in str(err)):
       port += 1
+      new_socket.close()
       return create_socket(port)
     else:
       raise Exception('[ERROR] Occurred during the UDP socket creation\n' + str(err))
